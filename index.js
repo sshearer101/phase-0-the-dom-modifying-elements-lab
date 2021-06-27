ad571620ae24cd32460c0ba558394aa2a26b529b
Sam Shearer <sshearer101@gmail.com>
2021-06-27 11:42:50 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement(`h1`);
+newHeader.setAttribute ("id", "victory");
+
+newHeader.innerHTML= `Scout is the champion`
+
+document.body.append(newHeader);
